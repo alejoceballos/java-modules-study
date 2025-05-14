ef92773b384fe77fcbbbaef50abc0668f1ac0b02
Alejo Ceballos <alejoceballos75@gmail.com>
2025-05-14 02:12:33 +0100

    Producing messages to two queues now

--- independent-producer/src/main/java/momo2x/study/javamodules/producer/independentproducer/Config.java
+++ independent-producer/src/main/java/momo2x/study/javamodules/producer/independentproducer/Config.java
@@ -4,40 +4,54 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Config {
 
-    private static final String QUEUE_NAME = "java-modules-study-queue";
+    private static final String QUEUE_NAME_1 = "java-modules-study-queue-1";
+    private static final String QUEUE_NAME_2 = "java-modules-study-queue-2";
 
     public static final String TOPIC_EXCHANGE_NAME = "java-modules-study-exchange";
 
-    @Bean
-    public String routingKey() {
-        return "momo2x.study.javamodules.#";
+    public static final String ROUTING_KEY_1 = "momo2x.study.javamodules.1.#";
+    public static final String ROUTING_KEY_2 = "momo2x.study.javamodules.2.#";
+
+    @Bean(QUEUE_NAME_1)
+    public Queue queue1() {
+        return new Queue(QUEUE_NAME_1, true);
     }
 
-    @Bean
-    public Queue queue() {
-        return new Queue(QUEUE_NAME, false);
+    @Bean(QUEUE_NAME_2)
+    public Queue queue2() {
+        return new Queue(QUEUE_NAME_2, true);
     }
 
-    @Bean
-    public TopicExchange exchange() {
+    @Bean(TOPIC_EXCHANGE_NAME)
+    public TopicExchange exchange2() {
         return new TopicExchange(TOPIC_EXCHANGE_NAME);
     }
 
-    @Bean
-    public Binding binding(
-            final String routingKey,
-            final Queue queue,
+    @Bean(ROUTING_KEY_1)
+    public Binding binding1(
+            @Qualifier(QUEUE_NAME_1) final Queue queue,
             final TopicExchange exchange) {
         return BindingBuilder
                 .bind(queue)
                 .to(exchange)
-                .with(routingKey);
+                .with(ROUTING_KEY_1);
+    }
+
+    @Bean(ROUTING_KEY_2)
+    public Binding binding2(
+            @Qualifier(QUEUE_NAME_2) final Queue queue,
+            final TopicExchange exchange) {
+        return BindingBuilder
+                .bind(queue)
+                .to(exchange)
+                .with(ROUTING_KEY_2);
     }
 
 //    @Bean
--- independent-producer/src/main/java/momo2x/study/javamodules/producer/independentproducer/Controller.java
+++ independent-producer/src/main/java/momo2x/study/javamodules/producer/independentproducer/Controller.java
@@ -8,6 +8,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
+import static momo2x.study.javamodules.producer.independentproducer.Config.ROUTING_KEY_1;
+import static momo2x.study.javamodules.producer.independentproducer.Config.ROUTING_KEY_2;
 import static momo2x.study.javamodules.producer.independentproducer.Config.TOPIC_EXCHANGE_NAME;
 import static org.springframework.http.HttpStatus.CREATED;
 
@@ -16,17 +20,26 @@ import static org.springframework.http.HttpStatus.CREATED;
 @RequestMapping("/producer")
 public class Controller {
 
-    private final String routingKey;
+    private static final String ROUTING_KEY_SUFFIX = "producer.sendToQueue";
+
     private final RabbitTemplate rabbitTemplate;
 
     @PostMapping
     public ResponseEntity<String> sendToQueue(@RequestBody final String body) {
-        final var sendRoutingKey = routingKey.replace("#", "producer.sendToQueue");
-        rabbitTemplate.convertAndSend(TOPIC_EXCHANGE_NAME, sendRoutingKey, body);
+        final var message = body + " - " + UUID.randomUUID();
+        rabbitTemplate.convertAndSend(
+                TOPIC_EXCHANGE_NAME,
+                ROUTING_KEY_1.replace("#", ROUTING_KEY_SUFFIX),
+                message + " (queue-1)");
+
+        rabbitTemplate.convertAndSend(
+                TOPIC_EXCHANGE_NAME,
+                ROUTING_KEY_2.replace("#", ROUTING_KEY_SUFFIX),
+                message + " (queue-2)");
 
         return ResponseEntity
                 .status(CREATED)
-                .body("Sent \"%s\" to \"%s\"".formatted(body, sendRoutingKey));
+                .body("Sent \"%s\" message".formatted(message));
     }
 
 }
